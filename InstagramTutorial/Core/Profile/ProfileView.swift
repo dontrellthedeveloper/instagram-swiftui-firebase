de2f3fd773df94191557b3716495855e3f971c1a
dontrellthedeveloper <dontrellknight@gmail.com>
2024-10-21 00:45:21 -0700

    added profile view

---
+++ InstagramTutorial/Core/Profile/ProfileView.swift
@@ -0,0 +1,103 @@
+//
+//  ProfileView.swift
+//  InstagramTutorial
+//
+//  Created by Dontrell Washington on 10/20/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                // header
+                VStack(spacing: 10) {
+                    // pic and stats
+                    HStack {
+                        Image("digital_leo_main")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStatView(value: 3, title: "Posts")
+                            
+                            UserStatView(value: 12, title: "Followers")
+                            
+                            UserStatView(value: 24, title: "Following")
+                        }
+                    }
+                    .padding(.horizontal)
+                    .padding(.bottom, 4)
+                    
+                    // name and bio
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Digital Leo")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Text("Follow @digital_leo for hot photography.")
+                            .font(.footnote)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    // action button
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundColor(.black)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color.gray, lineWidth: 1))
+                        
+                    }
+                    
+                    Divider()
+                    
+                }
+                
+                // post grid view
+                LazyVGrid(columns: gridItems, spacing: 1) {
+                    ForEach(0 ... 15, id: \.self) { index in
+                        Image("digital_leo_main")
+                            .resizable()
+                            .scaledToFill()
+                    }
+                }
+                
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundStyle(.black)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
