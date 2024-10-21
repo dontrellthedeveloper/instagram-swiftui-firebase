a210371601c571598da339e698daf5ce6b7b4c29
dontrellthedeveloper <dontrellknight@gmail.com>
2024-10-21 12:43:31 -0700

    added feed view

---
+++ InstagramTutorial/Core/Feed/FeedCell.swift
@@ -0,0 +1,97 @@
+//
+//  FeedCell.swift
+//  InstagramTutorial
+//
+//  Created by Dontrell Washington on 10/21/24.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack {
+            // image & username
+            HStack {
+                Image("digital_leo_main")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.red, lineWidth: 2))
+                
+                Text("digital_leo")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            
+            // post image
+            Image("digital_leo_2")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
+            
+            // action buttons
+            HStack(spacing: 16) {
+                Button {
+                    print("Like post")
+                } label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("Comment on post")
+                } label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("Share post")
+                } label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            .padding(.top, 4)
+            .foregroundStyle(.black)
+            
+            
+            // likes label
+            Text("123 likes")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+            
+            // caption label
+            HStack {
+                Text("digital_leo ").fontWeight(.semibold) +
+                Text("BTS of a recent celeb shoot 📸")
+            }
+            .font(.footnote)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 10)
+            .padding(.top, 1)
+            
+            Text("6h ago")
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+                .foregroundStyle(.gray)
+        }
+    }
+}
+
+#Preview {
+    FeedCell()
+}
