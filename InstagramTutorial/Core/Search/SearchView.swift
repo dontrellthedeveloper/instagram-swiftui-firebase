2b823ce076c912ab863ca08436b2ef586bdd9c32
dontrellthedeveloper <dontrellknight@gmail.com>
2024-10-21 22:49:25 -0700

    added search view

---
+++ InstagramTutorial/Core/Search/SearchView.swift
@@ -0,0 +1,50 @@
+//
+//  SearchView.swift
+//  InstagramTutorial
+//
+//  Created by Dontrell Washington on 10/21/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @State private var searchText: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach (0...15, id: \.self) {user in
+                        HStack {
+                            Image("digital_leo_main")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("digital_leo")
+                                    .fontWeight(.semibold)
+                                
+                                Text("Camron Lewis")
+                            }
+                            .font(.footnote)
+                            
+                            Spacer()
+                            
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}
