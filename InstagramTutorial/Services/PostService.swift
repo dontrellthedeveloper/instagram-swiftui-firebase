06576b6203a564e7340ad57b6b66fa5484463481
dontrellthedeveloper <dontrellknight@gmail.com>
2024-11-10 02:30:10 -0500

    added the ability to like posts on the server side

--- InstagramTutorial/Services/PostService.swift
+++ InstagramTutorial/Services/PostService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Firebase
+import FirebaseAuth
 
 struct PostService {
     
@@ -29,5 +30,32 @@ struct PostService {
     static func fetchUserPosts(uid: String) async throws -> [Post] {
         let snapshot = try await postsCollection.whereField("ownderUid", isEqualTo: uid).getDocuments()
         return try snapshot.documents.compactMap({ try $0.data(as: Post.self)})
+    }
+}
+
+
+extension PostService {
+    
+    static func likePost(_ post: Post) async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        async let _ = try await postsCollection.document(post.id).collection("post-likes").document(uid).setData([:])
+        async let _ = try await postsCollection.document(post.id).updateData(["likes": post.likes + 1])
+        async let _ = Firestore.firestore().collection("users").document(uid).collection("user-likes").document(post.id).setData([:])
+    }
+    
+    static func unlikePost(_ post: Post) async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        async let _ = try await postsCollection.document(post.id).collection("post-likes").document(uid).delete()
+        async let _ = try await postsCollection.document(post.id).updateData(["likes": post.likes - 1])
+        async let _ = Firestore.firestore().collection("users").document(uid).collection("user-likes").document(post.id).delete()
+    }
+    
+    static func checkIfUserLikedPost(_ post: Post) async throws -> Bool {
+        guard let uid = Auth.auth().currentUser?.uid else { return false }
+        
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).collection("user-likes").document(post.id).getDocument()
+        return snapshot.exists
     }
 }
